146a69b3e6c82157af0b837e8d177360273a67a7
muriol <test@test.com>
2023-12-19 18:43:27 +0900

    12-19 update insert 퀴즈

--- src/main/java/com/quiz/lesson03/bo/RealEstateBO.java
+++ src/main/java/com/quiz/lesson03/bo/RealEstateBO.java
@@ -23,4 +23,13 @@ public class RealEstateBO {
 	public List<RealEstate> getRealEstateByareaPrice(int area, int price) {
 		return realEstateMapper.selectRealEstateByareaPrice(area, price);
 	}
+	
+	public int addRealEstate(RealEstate realEstate) {
+		return realEstateMapper.insertRealEstate(realEstate);
+	}
+	//"썅떼빌리버 오피스텔 814호",45, "월세", 100000, 120
+	public int addRealEstateASField(int realtorId,String address, int area, 
+			String type, int price, int rentPrice) {
+		return realEstateMapper.insertRealEstateASField(realtorId, address, area, type, price, rentPrice);
+	}
 }
--- src/main/java/com/quiz/lesson03/mappers/RealEstateMapper.java
+++ src/main/java/com/quiz/lesson03/mappers/RealEstateMapper.java
@@ -2,6 +2,7 @@ package com.quiz.lesson03.mappers;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import com.quiz.lesson03.domain.RealEstate;
@@ -9,6 +10,23 @@ import com.quiz.lesson03.domain.RealEstate;
 @Repository
 public interface RealEstateMapper {
 	public RealEstate selectRealEstateById(int id);
+	
 	public List<RealEstate> selectRealEstateByrentPrice(Integer rentPrice);
-	public List<RealEstate> selectRealEstateByareaPrice(int area, int price);
+	
+	public List<RealEstate> selectRealEstateByareaPrice(
+			// myBatis 문법상 파라미터는 단 한개만 xml 파일로 보낼수있다.
+			// 여러개일시엔 하나의 맵으로 묶어서 보낸다.
+			// @Param
+			@Param("area") int area,
+			@Param("price") int price);
+	
+	public int insertRealEstate(RealEstate realEstate);
+	
+	public int insertRealEstateASField(
+			@Param("realtorId") int realtorId,
+			@Param("address") String address, 
+			@Param("area") int area, 
+			@Param("type") String type,
+			@Param("price") int price, 
+			@Param("rentPrice") int rentPrice);
 }
